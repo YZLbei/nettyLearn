cb99689c1717dd2d5abc70a3f5c7f1353f3a2ffe
YZLBei <yuzhenlongbei@outlook.com>
2022-05-23 20:21:08 +0800

    24p-25p nio非阻塞模式

--- src/test/java/netty/c4/Server.java
+++ src/test/java/netty/c4/Server.java
@@ -23,6 +23,7 @@ public class Server {
         
         //1.创建服务器
         ServerSocketChannel ssc  = ServerSocketChannel.open();
+        ssc.configureBlocking(false);//非阻塞模式,影响的是accept方法
         
         //2.绑定端口
         ssc.bind(new InetSocketAddress(8080));
@@ -34,11 +35,18 @@ public class Server {
             //4.建立连接
             log.debug("Connecting:");
             SocketChannel accept = ssc.accept();
-            log.debug("Connected:{}",accept);
-            channels.add(accept);//阻塞方法，会让线程暂停，线程停止运行
+            if (accept!=null) {
+                log.debug("Connected:{}", accept);
+                accept.configureBlocking(false);//将read方法设置为非阻塞的
+                //非阻塞模式下，线程会继续运行如果没有连接建立，accept为null 
+                channels.add(accept);//阻塞方法，会让线程暂停，线程停止运行
 
+            }
+            
             for (SocketChannel channel : channels) {
                 log.debug("before read{}",channel);
+                
+                //设置成非阻塞，线程会继续运行，如果没有读到数据read返回0
                 channel.read(buffer);//read方法也是阻塞，线程也会停止运行
                 buffer.flip();
                 debugRead(buffer);
